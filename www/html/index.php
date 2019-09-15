882378e5265260a649210e13e2673af01edec7ea
atsu0131 <atsushi.isai131@gmail.com>
2019-09-16 01:47:27 +0900

    add

--- www/html/index.php
+++ www/html/index.php
@@ -1,6 +1,44 @@
+<body>
 
+<h1>九九表</h1>
+<table>
+<tr><th>&nbsp;</th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th></tr>
 <?php
+for ( $i = 1; $i <= 9; $i++ ) {
 
-for ($i = 1; $i <= 10; $i++) {
-  echo $i;
+  print '<tr>';
+  print '<th>' . $i . '</th>';
+
+    for ( $j = 1; $j <= 9; $j++ ) {
+      print '<td>' . $i*$j . '</td>';
+    }
+  print '</tr>' . "\n";
 }
+?>
+</table>
+</body>
+
+<style>
+body {
+  font-family:
+  "Hiragino Kaku Gothic Pro N",
+  Meiryo,
+  sans-serif;
+}
+table {
+  border-collapse: collapse;
+  border: 1px solid coral;
+}
+th, td {
+  width: 50px;
+  border: 1px dotted coral;
+
+}
+th {
+  background: lightpink;
+}
+td {
+  text-align: center;
+}
+</style>
+
